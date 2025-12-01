5817152c0cd47da67064ce665f5b58665682bb14
esplusnin <esplusnin94@yandex.ru>
2025-12-01 16:40:38 +0300

    Initial Commit

---
+++ GloveBox/GloveBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloveBoxApp.swift
+//  GloveBox
+//
+//  Created by Евгений on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct GloveBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
